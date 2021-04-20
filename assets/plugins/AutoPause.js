8a3293b6fc8360c40d5858d74614aa55df30140c
kathecr <lkcastelblancor@correo.udistrital.edu.co>
2021-04-19 23:14:42 -0500

    Add event visibilityChange to autoplay or autopause

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -1,20 +1,28 @@
 class AutoPause {
   constructor() {
     this.threshold = 0.25;
-    this.handleIntersection = this.handleIntersection.bind(this)
+    this.handleIntersection = this.handleIntersection.bind(this);
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
   }
   run(player) {
     this.player = player;
-    const observer = new IntersectionObserver(
-      this.handleIntersection,
-      {threshold: this.threshold}
-    );
+    const observer = new IntersectionObserver(this.handleIntersection, {
+      threshold: this.threshold,
+    });
     observer.observe(this.player.media);
+    document.addEventListener("visibilitychange", this.handleVisibilityChange);
   }
   handleIntersection(entries) {
     const entry = entries[0];
     console.log(entry);
-    entry.intersectionRatio < this.threshold ? this.player.pause() : this.player.play();
+    entry.intersectionRatio < this.threshold
+      ? this.player.pause()
+      : this.player.play();
+  }
+  handleVisibilityChange() {
+    document.visibilityState === "visible"
+      ? this.player.play()
+      : this.player.pause();
   }
 }
 
