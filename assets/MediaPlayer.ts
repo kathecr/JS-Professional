65d97982da1ad1a45232dfe92b337792fd1e3aff
kathecr <kcastelblancor@gmail.com>
2021-04-21 15:03:56 -0500

    Add ads to the media video each 30 seconds

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -1,12 +1,20 @@
 import AutoPlay from "./plugins/AutoPlay";
-
 class MediaPlayer {
   media: HTMLMediaElement;
   plugins: Array<any>;
+  container: HTMLElement;
+
   constructor(config) {
     this.media = config.el;
     this.plugins = config.plugins || [new AutoPlay()];
+    this.initPlayer();
     this.initPlugins();
+  }
+  initPlayer() {
+    this.container = document.createElement('div');
+    this.container.style.position = 'relative';
+    this.media.parentNode.insertBefore(this.container, this.media)
+    this.container.appendChild(this.media)
   }
   private initPlugins() {
     this.plugins.forEach(plugin => {
